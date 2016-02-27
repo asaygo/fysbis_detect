2154d86fca7e4f7230ef9d3bfb011f24c034c91e
asaygo <andrei.saygo@gmail.com>
2016-02-26 22:41:01 -0800

    add support for 32, add more signature fragments, add signature sensitivity

--- fysbis_scan.py
+++ fysbis_scan.py
@@ -1,4 +1,10 @@
 #! python
+#####################################
+#	Copyright LF-2095				#
+#	Developed for Linux Forensics	#
+#	course on pentesteracademy.com	#
+#####################################
+
 import struct
 import os
 import binascii
@@ -7,26 +13,22 @@ import sys
 
 MAX_FILE_SIZE = 200000
 MIN_FILE_SIZE = 100000
-HEADER_SIZE	  = 64
 F_CLEAN		  = 0
 F_INFECTED	  = 1
 
 SIGNATURE = [
 				"73797374656d63746c2064697361626c65",	#systemctl disable
-				"2f6c69622f6376612d73737973"			#/lib/cva-ssys
+				"2f6c69622f6376612d73737973",			#/lib/cva-ssys
+				"746172676574",							#target
+				"636f6e6669672f6175746f7374617274"		#config/autostart
 			]
 
-elfMagic = "7F454C46"
+#to increase the heuristic, reduce the SENSITIVITY
+SENSITIVITY	  = len(SIGNATURE)
 
-flag_elf = 0
-flag_64bit = 0
-flag_32bit = 0
+elfMagic = "7F454C46"
 fsize = 0
 buf = None
-entry_point = 0
-program_header_offset = 0
-section_header_offset = 0
-header_size = 0
 
 def check_size():
 	global MIN_FILE_SIZE
@@ -38,22 +40,21 @@ def check_size():
 	return fsize
 
 def scan_file():
-	global buf
-	global fsize
 	global elfMagic
-	global flag_64bit
-	global flag_32bit
-	global entry_point
-	global program_header_offset
-	global section_header_offset
-	global header_size
-	flag_infected = 0
-	pheader_entry_sz	= 0
-	pheader_entry_no	= 0
-	sheader_entry_sz	= 0
-	sheader_entry_no	= 0
-	shstrtab_index		= 0
-
+	global fsize	
+	entry_point 			= 0
+	program_header_offset 	= 0
+	section_header_offset 	= 0
+	header_size 			= 0
+	flag_infected 			= 0
+	flag_elf 				= 0
+	flag_64bit 				= 0
+	flag_32bit 				= 0
+	pheader_entry_sz		= 0
+	pheader_entry_no		= 0
+	sheader_entry_sz		= 0
+	sheader_entry_no		= 0
+	shstrtab_index			= 0
 
 	print ("File: " + str(sys.argv[1]))
 
@@ -117,52 +118,96 @@ def scan_file():
 					aux = buf[62:64]
 					val = struct.unpack('H', aux);
 					shstrtab_index = hex(val[0])
-
-				else:
+					
+			else:
+				if endian == "01":
 					aux = buf[24:28]
-					val = struct.unpack('Q', aux);
-					entry_point = hex(val[0])					
-			
-			
+					val = struct.unpack('L', aux);
+					entry_point = hex(val[0])
+										
+					aux = buf[28:32]
+					val = struct.unpack('L', aux);
+					program_header_offset = hex(val[0])
+					
+					aux = buf[32:36]
+					val = struct.unpack('L', aux);
+					section_header_offset = hex(val[0])
+					
+					aux = buf[40:42]
+					val = struct.unpack('H', aux);
+					header_size = hex(val[0])
+					
+					aux = buf[42:44]
+					val = struct.unpack('H', aux);
+					pheader_entry_sz = val[0]
+					
+					aux = buf[44:46]
+					val = struct.unpack('H', aux);
+					pheader_entry_no = val[0]
+					
+					aux = buf[46:48]
+					val = struct.unpack('H', aux);
+					sheader_entry_sz = hex(val[0])
+
+					aux = buf[48:50]
+					val = struct.unpack('H', aux);
+					sheader_entry_no = hex(val[0])
+
+					aux = buf[50:52]
+					val = struct.unpack('H', aux);
+					shstrtab_index = hex(val[0])
+						
 			max_load_addr = 0
 			max_file_offset = 0
 			start_offset = 64
 			load_addr = 0
-			for i in range(0,int(pheader_entry_no)):						
-				aux = buf[i*pheader_entry_sz+start_offset+8:i*pheader_entry_sz+start_offset+16]
-				val = struct.unpack('Q', aux);
-				file_offset = val[0]
+			for i in range(0,int(pheader_entry_no)):
+				if (flag_64bit == 1):
+					aux = buf[i*pheader_entry_sz+start_offset+8:i*pheader_entry_sz+start_offset+16]
+					val = struct.unpack('Q', aux);
+					file_offset = val[0]
 						
-				aux = buf[i*pheader_entry_sz+start_offset+16:i*pheader_entry_sz+start_offset+24]
-				val = struct.unpack('Q', aux);
-				load_addr = val[0]
+					aux = buf[i*pheader_entry_sz+start_offset+16:i*pheader_entry_sz+start_offset+24]
+					val = struct.unpack('Q', aux);
+					load_addr = val[0]
+
+				else:
+					aux = buf[i*pheader_entry_sz+start_offset+8:i*pheader_entry_sz+start_offset+12]
+					val = struct.unpack('L', aux);
+					file_offset = val[0]
+						
+					aux = buf[i*pheader_entry_sz+start_offset+12:i*pheader_entry_sz+start_offset+16]
+					val = struct.unpack('L', aux);
+					load_addr = val[0]
 						
 				if load_addr < entry_point and load_addr > max_load_addr:
 					max_load_addr = load_addr
 					max_file_offset = file_offset
 					
-			if max_load_addr > 0 and max_file_offset > 0:
-				print ("Section load addr: " + str(max_load_addr))
-				print ("Section file offset: " + str(max_file_offset))
-			else:
+			if max_load_addr < 0 or max_file_offset < 0:
 				return F_CLEAN	
-			
-			#if (flag_64bit == 1):
-			#	print ("64bit file")
-			
+						
 			if max_file_offset < fsize:
 				f.seek(max_file_offset)
 				
 				strbuf = binascii.hexlify(buf)
+				count = 0
 				for index in range(len(SIGNATURE)):
 					pos = strbuf.find(SIGNATURE[index], max_file_offset)
 					#print "Malware identified at offset: " + str(pos)
 					if ( pos > 0):
-						return F_INFECTED
+						count = count +1
+				
+				if (count >= SENSITIVITY):
+					return F_INFECTED
 				
 		return F_CLEAN
 
 ###MAIN###
+if len(sys.argv) != 2:
+	print ("Program <file name>")
+	sys.exit(0)
+	
 fsize = check_size()
 if (fsize == 0):
 	print ("Incorrect file size")
